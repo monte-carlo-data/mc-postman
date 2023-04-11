4c7a17f74daacd0417d1bdee6e1b94e7f019c189
Melissa <mhale@montecarlodata.com>
2023-04-10 19:53:08 -0500

    push

--- graphman/src/utils/fetch-introspection.ts
+++
@@ -1,6 +0,0 @@
-import { fetchIntrospection, saveJsonFormatted } from "../lib.ts";
-
-const url = "https://rickandmortyapi.com/graphql";
-const introspection = await fetchIntrospection(url);
-// const {queryType, mutationType} = getQueryAndMutationTypes(introspection);
-saveJsonFormatted(introspection, "./out/introspection.json");
